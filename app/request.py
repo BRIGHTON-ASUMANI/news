7e035987f870e709d5f42e8cf7386371448e6067
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-08-31 17:14:26 +0300

    i created the run function in the manage.py module and also set the sources as the first priority in request module

--- app/request.py
+++ app/request.py
@@ -32,52 +32,6 @@ def get_from_source(articles,source):
 
     return article_results
 
-def get_articles(articles):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_articles_url = base_url.format(articles,api_key)
-    # print(get_articles_url)
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
-
-        article_results = None
-
-        if get_articles_response['articles']:
-            article_results_list = get_articles_response['articles']
-            article_results = process_articles(article_results_list)
-
-
-    return article_results
-
-
-def process_articles(article_list):
-    '''
-    Function  that processes the article result and transform them to a list of Objects
-
-    Args:
-        article_list: A list of dictionaries that contain article details
-
-    Returns :
-        article_results: A list of article objects
-    '''
-    article_results = []
-    for article_item in article_list:
-        title = article_item.get('title')
-        author = article_item.get('author')
-        description = article_item.get('description')
-        urlToImage = article_item.get('urlToImage')
-        url = article_item.get('url')
-        publishedAt = article_item.get('publishedAt')
-
-
-        if urlToImage:
-            article_object = Articles(title,author,description,urlToImage,url,publishedAt)
-            article_results.append(article_object)
-
-    return article_results
-
 
 def get_sources(sources):
     '''
@@ -123,3 +77,49 @@ def process_sources(source_list):
         source_results.append(source_object)
 
     return source_results
+
+def get_articles(articles):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base_url.format(articles,api_key)
+    # print(get_articles_url)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        article_results = None
+
+        if get_articles_response['articles']:
+            article_results_list = get_articles_response['articles']
+            article_results = process_articles(article_results_list)
+
+
+    return article_results
+
+
+def process_articles(article_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+
+    Args:
+        article_list: A list of dictionaries that contain article details
+
+    Returns :
+        article_results: A list of article objects
+    '''
+    article_results = []
+    for article_item in article_list:
+        title = article_item.get('title')
+        author = article_item.get('author')
+        description = article_item.get('description')
+        urlToImage = article_item.get('urlToImage')
+        url = article_item.get('url')
+        publishedAt = article_item.get('publishedAt')
+
+
+        if urlToImage:
+            article_object = Articles(title,author,description,urlToImage,url,publishedAt)
+            article_results.append(article_object)
+
+    return article_results
