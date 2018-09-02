1c6e177e8ddf4664c4ca2a8471fbf1280d775618
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-02 23:26:03 +0300

    added some styling in thumbnails and all attributes related to the index template

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,6 +1,10 @@
-jQuery(document).ready(function () {
+$("#slideshow > div:gt(0)").hide();
 
-
-    $('#carouselHacked').carousel();
-
-    });
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
