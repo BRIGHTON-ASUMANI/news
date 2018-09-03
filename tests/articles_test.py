4023207989632817415e8d84e1586bfc6f08c529
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-04 01:44:36 +0300

    created two test one for articles and the other one for sources

--- tests/articles_test.py
+++ tests/articles_test.py
@@ -1,11 +1,30 @@
 import unittest # Importing the unittest module
-from ..contact import Contact # Importing the contact class
+from app.models import Articles, Sources # Importing the contact class
 
-class TestSoures(unittest.TestCase):
-
+class TestArticles(unittest.TestCase):
     '''
-    Test class that defines test cases for the contact class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
+    Test Class to test the behaviour of the Articles class
     '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_articles = Articles('Floods','cnn','life in bangladesh','image','8.5129993')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles,Articles))
+
+class TestSources(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Sources class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_sources = Sources(1,'name','life in bangladesh','www.bbc.com','general','english','country')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_sources,Sources))
