0e134ad7d363cfc89ef280ba0d57d77702e39d25
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-04 11:33:40 +0300

    added multiple templates and their routing. realtionship  made with the navabar

--- app/main/views.py
+++ app/main/views.py
@@ -44,3 +44,33 @@ def news(news_id):
     View news page function that returns the news details page and its data
     '''
     return render_template('news.html',id = news_id)
+
+@main.route('/general')
+def general():
+    general = get_categories('general')
+    return render_template('general.html', general = general)
+
+@main.route('/business')
+def business():
+    business = get_categories('business')
+    return render_template('business.html', business = business)
+
+@main.route('/sports')
+def sports():
+    sports = get_categories('sports')
+    return render_template('sports.html', sports = sports)
+
+@main.route('/technology')
+def technology():
+    technology = get_categories('technology')
+    return render_template('technology.html', technology = technology)
+
+@main.route('/health')
+def health():
+    health = get_categories('health')
+    return render_template('health.html', health = health)
+
+@main.route('/science')
+def science():
+    science = get_categories('science')
+    return render_template('science.html', science = science)
